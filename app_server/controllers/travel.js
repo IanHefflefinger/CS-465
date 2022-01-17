34d9fa4720618f72ae97c0eadf5da3fa62b407d0
ian hefflefinger <ian.hefflefinger@snhu.edu>
2022-01-16 20:13:12 -0700

    Convert travel template from static data to dynamic JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,10 @@
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /* GET Travel View */
 const travel = (req, res) => {
     pageTitle = process.env.npm_package_description + ' - Travel'; // process.env.npm_package_description can only be accessed if you use 'npm start' *nodemon will not work
-    res.render('travel', { title: 'Travlr Getaways' });
+    res.render('travel', { title: pageTitle, trips });
 };
 
 module.exports = {
