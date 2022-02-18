7be53b8d80afcd7fc1c13b2ffc6d6301d3020b23
ian hefflefinger <ian.hefflefinger@snhu.edu>
2022-02-17 17:37:51 -0700

    this code does not work

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+require('dotenv').config();
+
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const hbs = require('hbs');
+const passport = require('passport');
+
 require('./app_api/database/db');
+require('./app_api/config/passport');
+
 
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
@@ -21,8 +27,8 @@ const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
-  // register handlebars partials (https://www.npmjs.com/package/hbs)
-  hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'))
+// register handlebars partials (https://www.npmjs.com/package/hbs)
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'))
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -30,11 +36,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 // allow CORS (Cross Origin Resource Sharing)
 app.use('/api', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
   next();
 });
@@ -49,6 +56,14 @@ app.use('/contact', contactRouter);
 app.use('/about', aboutRouter);
 app.use('/api', apiRouter);
 
+// catch unauthorized error and create 401
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res
+      .status(401)
+      .json({"message": err.name + ": " + err.message});
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app_api/database/db.js
+++ app_api/database/db.js
@@ -69,5 +69,6 @@ process.on("SIGTERM", () => {
 
 connect();
 
-// bring in schema
+// bring in schema(s)
 require("./models/travlr");
+require("./models/user");
